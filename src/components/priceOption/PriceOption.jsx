2450ef2d8f28e83f189b9541c76ad8fe3cc8b816
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-19 14:22:39 +0600

    card section complite

--- src/components/priceOption/PriceOption.jsx
+++ src/components/priceOption/PriceOption.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
 const PriceOption = ({option}) => {
+
+    const {name, price, features} = option;
     return (
-        <div>
-            <h1>price option:{option.length}</h1>
+        <div className='bg-blue-400 text-black p-5 rounded-lg'>
+            <span className='text-6xl font-bold text-center'>{price}</span>
+            <span className='text-xl font-bold text-center'>/mon</span>
+            <h2 className='text-4xl font-bold text-center'>{name}</h2>
+            {
+                features.map((item, indx) =><li>{item}</li>)
+            }
         </div>
     );
 };
