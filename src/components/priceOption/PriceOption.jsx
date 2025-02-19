518cc9264dad073666a8099a03efc36792ac3bd1
Mr. Bharat Roy <bharatroy1005@gmail.comgidjfclsgir>
2025-02-19 17:06:42 +0600

    card into add button

--- src/components/priceOption/PriceOption.jsx
+++ src/components/priceOption/PriceOption.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 
-const PriceOption = ({option}) => {
+const PriceOption = ({ option }) => {
 
-    const {name, price, features} = option;
+    const { name, price, features } = option;
     return (
-        <div className='bg-blue-400 text-black p-5 rounded-lg'>
+        <div className='bg-blue-400 text-black p-5 flex flex-cojl rounded-lg'>
             <span className='text-6xl font-bold text-center'>{price}</span>
             <span className='text-xl font-bold text-center'>/mon</span>
             <h2 className='text-4xl font-bold text-center'>{name}</h2>
-            {
-                features.map((item, indx) =><li>{item}</li>)
-            }
+            <div className='flex-grow'>
+                {
+                    features.map((item, indx) => <li>{item}</li>)
+                }
+            </div>
+            <button className='w-full bg-green-400 py-3 mt-4 rounded-md'>Buy Now</button>
         </div>
     );
 };
